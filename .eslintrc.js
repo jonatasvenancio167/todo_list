fc77df6bb328cf9369094787e58a2cbb9ccce9d3
jonatas venancio <jvenancio167@hotmail.com>
2024-07-04 06:41:32 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['@react-native-community'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+
+      // Apply the recommended Typescript defaults and the prettier overrides to all Typescript files
+      rules: Object.assign(typescriptEslintRecommended.rules, typescriptEslintPrettier.rules, {
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        'react-native/no-inline-styles': 'off',
+      }),
+    },
+  ],
+}
